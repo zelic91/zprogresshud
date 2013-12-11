c5f4927c3f586202363a68a79d7e715de78fc5bc
Hoai Thuong <thuongnh.uit@gmail.com>
2013-12-11 10:24:45 +0700

    Fix for invalid window token[C

--- ZProgressHUD/src/com/thuongnh/zprogresshud/ZProgressHUD.java
+++ ZProgressHUD/src/com/thuongnh/zprogresshud/ZProgressHUD.java
@@ -1,5 +1,6 @@
 package com.thuongnh.zprogresshud;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
@@ -14,7 +15,7 @@ import com.example.zprogresshud.R;
 
 public class ZProgressHUD extends Dialog {
 
-	public static final int FADE_ROUND_SPINNER = 0;
+	public static final int FADED_ROUND_SPINNER = 0;
 	public static final int GEAR_SPINNER = 1;
 	public static final int SIMPLE_ROUND_SPINNER = 2;
 
@@ -23,8 +24,9 @@ public class ZProgressHUD extends Dialog {
 	TextView tvMessage;
 	ImageView ivSuccess;
 	ImageView ivFailure;
-	ImageView ivprogressSpinner;
+	ImageView ivProgressSpinner;
 	AnimationDrawable adProgressSpinner;
+	Context context;
 
 	public static ZProgressHUD getInstance(Context context) {
 		if (instance == null) {
@@ -37,38 +39,48 @@ public class ZProgressHUD extends Dialog {
 		super(context, R.style.DialogTheme);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.setCanceledOnTouchOutside(false);
+		this.context = context;
 		view = getLayoutInflater().inflate(R.layout.dialog_progress, null);
 		tvMessage = (TextView) view.findViewById(R.id.textview_message);
 		ivSuccess = (ImageView) view.findViewById(R.id.imageview_success);
 		ivFailure = (ImageView) view.findViewById(R.id.imageview_failure);
-		ivprogressSpinner = (ImageView) view
+		ivProgressSpinner = (ImageView) view
 				.findViewById(R.id.imageview_progress_spinner);
 
-		setSpinnerType(FADE_ROUND_SPINNER);
+		setSpinnerType(FADED_ROUND_SPINNER);
 		this.setContentView(view);
 	}
 
 	public void setSpinnerType(int spinnerType) {
 		switch (spinnerType) {
 		case 0:
-			ivprogressSpinner.setImageResource(R.anim.round_spinner_fade);
+			ivProgressSpinner.setImageResource(R.anim.round_spinner_fade);
 			break;
 		case 1:
-			ivprogressSpinner.setImageResource(R.anim.gear_spinner);
+			ivProgressSpinner.setImageResource(R.anim.gear_spinner);
 			break;
 		case 2:
-			ivprogressSpinner.setImageResource(R.anim.round_spinner);
+			ivProgressSpinner.setImageResource(R.anim.round_spinner);
 			break;
 		default:
-			ivprogressSpinner.setImageResource(R.anim.round_spinner_fade);
+			ivProgressSpinner.setImageResource(R.anim.round_spinner_fade);
 		}
 
-		adProgressSpinner = (AnimationDrawable) ivprogressSpinner.getDrawable();
+		adProgressSpinner = (AnimationDrawable) ivProgressSpinner.getDrawable();
 		adProgressSpinner.start();
 	}
-	
+
 	public void setMessage(String message) {
 		tvMessage.setText(message);
+	}
+
+	@Override
+	public void show() {
+		if (!((Activity) context).isFinishing()) {
+			super.show();
+		} else {
+			instance = null;
+		}
 	}
 
 	public void dismissWithSuccess() {
@@ -104,17 +116,17 @@ public class ZProgressHUD extends Dialog {
 	}
 
 	protected void showSuccessImage() {
-		ivprogressSpinner.setVisibility(View.GONE);
+		ivProgressSpinner.setVisibility(View.GONE);
 		ivSuccess.setVisibility(View.VISIBLE);
 	}
 
 	protected void showFailureImage() {
-		ivprogressSpinner.setVisibility(View.GONE);
+		ivProgressSpinner.setVisibility(View.GONE);
 		ivFailure.setVisibility(View.VISIBLE);
 	}
 
 	protected void reset() {
-		ivprogressSpinner.setVisibility(View.VISIBLE);
+		ivProgressSpinner.setVisibility(View.VISIBLE);
 		ivFailure.setVisibility(View.GONE);
 		ivSuccess.setVisibility(View.GONE);
 		tvMessage.setText("Loading ...");
